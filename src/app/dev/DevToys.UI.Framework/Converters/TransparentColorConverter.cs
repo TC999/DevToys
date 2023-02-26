99450b77b77750c6cc29f7943b59d1a7d585e18f
Etienne BAUDOUX <ebaudoux@velersoftware.com>
2023-02-26 14:24:20 -0800

    Added All Tools page backgroun (WIP)

---
+++ src/app/dev/DevToys.UI.Framework/Converters/TransparentColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Windows.UI;
+
+namespace DevToys.UI.Framework.Converters;
+
+/// <summary>
+/// Convert a <see cref="Color"/> to a <see cref="Color"/> with full transparency.
+/// </summary>
+public sealed class TransparentColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        var color = value as Color?;
+        if (color == null)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+
+        return Color.FromArgb(0, color.Value.R, color.Value.G, color.Value.B);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
